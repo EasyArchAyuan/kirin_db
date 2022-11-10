92ea42d27124e8f5c04e18f727c1a424a1bde419
Ayuan <909897268@qq.com>
2022-11-10 22:22:47 +0800

    feat: record_page实现

--- record_manager/interface.go
+++ record_manager/interface.go
@@ -1,5 +1,8 @@
 package record_manager
 
+import fm "file_manager"
+
+// SchemaInterface 元数据接口
 type SchemaInterface interface {
 	AddField(field_name string, field_type FIELD_TYPE, length int)
 	AddIntField(field_name string)
@@ -12,8 +15,22 @@ type SchemaInterface interface {
 	Length(field_name string) int
 }
 
+// LayoutInterface 字段信息接口
 type LayoutInterface interface {
 	Schema() SchemaInterface
 	Offset(field_name string) int
 	SlotSize() int
+}
+
+// RecordManagerInterface 记录管理器接口
+type RecordManagerInterface interface {
+	Block() *fm.BlockId                                //返回记录所在页面对应的区块
+	GetInt(slot int, field_name string) int            //根据给定字段名取出其对应的int值
+	SetInt(slot int, field_name string, val int)       //设定指定字段名的int值
+	GetString(slot int, field_name string) string      //根据给定字段名获取其字符串内容
+	SetString(slot int, field_name string, val string) //设置给定字段名的字符串内容
+	Format()                                           //将所有插槽中的记录设定为默认值
+	Delete(slot int)                                   //将给定插槽的占用标志位设置为0
+	NextAfter(slot int)                                //查找给定插槽之后第一个占用标志位为1的记录
+	InsertAfter(slot int)                              //查找给定插槽之后第一个占用标志位为0的记录
 }
